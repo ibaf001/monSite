ad2feb6f2093bca19acd116e31109840c950e88a
ibo <ibobafumba@gmail.com>
2017-01-04 22:11:15 -0500

    Added bootstrap

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
@@ -9,6 +11,10 @@ def index():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+
+@app.route("/euler")
+def euler():
+	return render_template("euler.html")
 
 
 if __name__ == '__main__':
